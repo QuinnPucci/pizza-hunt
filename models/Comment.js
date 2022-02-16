9e6643d328de714e3d7f10fcff0d2420e4706289
QuinnPooch <pucciquinn@gmail.com>
2022-02-16 16:41:27 -0700

    added front end JS for reply feature, everything tested and working

--- models/Comment.js
+++ models/Comment.js
@@ -1,62 +1,61 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema(
-    {  
-    writtenBy: {
+const ReplySchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment _id
+      replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      replyBody: {
         type: String
-    },
-    commentBody: {
+      },
+      writtenBy: {
         type: String
-    },
-    createdAt: {
+      },
+      createdAt: {
         type: Date,
         default: Date.now,
         get: createdAtVal => dateFormat(createdAtVal)
+      }
     },
-    // validate data for a reply through reply schema
-    replies: [ReplySchema]
- },
- {
-     toJSON: {
-         virtuals: true,
-         getters: true
-     },
-     id: false
- }
-)
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  )
+
+  const CommentSchema = new Schema(
+    {
+      writtenBy: {
+        type: String
+      },
+      commentBody: {
+        type: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      // use ReplySchema to validate data for a reply
+      replies: [ReplySchema]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  );
 
 CommentSchema.virtual('replyCount').get(function(){
     return this.replies.length
 })
 
-const ReplySchema = new Schema(
-    {
-        // set custom ID to avoid confusion with with parent comment _id
-        replyId: {
-            type: Schema.Types.ObjectId,
-            default: () => Types.ObjectId
-        },
-        replyBody: {
-            type: String
-        },
-        writtenBy: {
-            type: String
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
-    },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-)
-
 const Comment = model('Comment', CommentSchema)
 
 module.exports = Comment
-module.exports = Reply
